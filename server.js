96b1703083e81a895fd59758661fd4a913a6e0cd
Yulia Liber bc2025-5 <liberyula07@gmail.com>
2025-11-02 23:34:55 -0800

    add DELETE

--- server.js
+++ server.js
@@ -90,6 +90,19 @@ const server = http.createServer(async (req, res) => {
     return;
   }
   
+  // DELETE
+  if (req.method === 'DELETE') {
+    try {
+      await fsp.unlink(filePath);
+      res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+      res.end('видалено');
+    } catch (e) {
+      res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+      res.end('нема');
+    }
+    return;
+  }
+
   res.writeHead(405, { 'Content-Type': 'text/plain; charset=utf-8' });
   res.end('метод не підтримується');
 });
